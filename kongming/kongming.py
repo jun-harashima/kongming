dec8c0fb338cb011983736118c0fe621cb825efa
Jun Harashima <jun-harashima@cookpad.com>
2020-05-18 21:11:25 +0900

    add test

--- kongming/kongming.py
+++ kongming/kongming.py
@@ -1,1 +1,63 @@
-"""Main module."""
+import CaboCha
+
+
+class Kongming:
+
+    def __init__(self, stopwords=None):
+        self.cabocha = CaboCha.Parser()
+        self.stopwords = stopwords
+
+    def _get_modifier(self, tree, chunk):
+        surface = ''
+        for i in range(chunk.token_pos, chunk.token_pos + chunk.head_pos + 1):
+            token = tree.token(i)
+            features = token.feature.split(',')
+            surface += token.surface
+        return surface
+
+    def _get_function(self, tree, chunk):
+        surface = ''
+        for i in range(chunk.token_pos + chunk.head_pos + 1, chunk.token_pos + chunk.token_size):
+            token = tree.token(i)
+            features = token.feature.split(',')
+            if not features[1] in ['読点', '句点']:
+                surface += token.surface
+        return surface
+
+    def _get_head(self, tree, chunk):
+        start = chunk.token_pos
+        end = start + chunk.head_pos + 1 if chunk.head_pos != chunk.func_pos else start + chunk.token_size
+        surface = ''
+        for i in range(start, end):
+            token = tree.token(i)
+            features = token.feature.split(',')
+            if self.stopwords and token.surface in self.stopwords:
+                continue
+            if features[0] in ['名詞', '形容詞', '記号']:
+                surface += token.surface
+            elif features[0] == '動詞':
+                surface += features[6]
+                break
+        return surface
+
+    def _extract_arrows(self, tree):
+        chunks = {}
+        for i in range(0, tree.chunk_size()):
+            chunks[i] = tree.chunk(i)
+
+        arrows = []
+        for chunk_id, chunk in chunks.items():
+            if not chunk.link > 0:
+                continue
+            modifier = self._get_modifier(tree, chunk)
+            function = self._get_function(tree, chunk)
+            head_chunk = chunks[chunk.link]
+            head = self._get_head(tree, head_chunk)
+            arrow = {"modifier": modifier, "function": function, "head": head}
+            arrows.append(arrow)
+        return arrows
+
+    def collect(self, text):
+        tree = self.cabocha.parse(text)
+        dependencies = self._extract_arrows(tree)
+        return dependencies
--- tests/test_kongming.py
+++ tests/test_kongming.py
@@ -1,33 +1,80 @@
-#!/usr/bin/env python
-
-"""Tests for `kongming` package."""
-
-
 import unittest
-from click.testing import CliRunner
-
-from kongming import kongming
-from kongming import cli
+from kongming.kongming import Kongming
 
 
 class TestKongming(unittest.TestCase):
-    """Tests for `kongming` package."""
+
+    def assertModifierEqual(self, tree, chunk_id, modifier):
+        _modifier = self.kongming._get_modifier(tree, tree.chunk(chunk_id))
+        self.assertEqual(_modifier, modifier)
+
+    def assertFunctionEqual(self, tree, chunk_id, function):
+        _function = self.kongming._get_function(tree, tree.chunk(chunk_id))
+        self.assertEqual(_function, function)
+
+    def assertHeadEqual(self, tree, chunk_id, head):
+        _head = self.kongming._get_head(tree, tree.chunk(chunk_id))
+        self.assertEqual(_head, head)
 
     def setUp(self):
-        """Set up test fixtures, if any."""
+        self.kongming = Kongming(stopword=["。"])
+        self.text1 = '鶏肉が揚がったら香味ソースをかけて出来上がり！'
+        self.text2 = '鶏のもも肉を適当な大きさに切って、酒・醤油に漬けておく。（1時間）'
+        self.text3 = 'すると出来上り。'
 
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
+    # we don't care about the last chunk because it doesn't modify other chunks
+    def test_get_modifier(self):
+        tree = self.kongming.cabocha.parse(self.text1)
+        self.assertModifierEqual(tree, 0, '鶏肉')
+        self.assertModifierEqual(tree, 1, '揚がっ')
+        self.assertModifierEqual(tree, 2, '香味ソース')
+        self.assertModifierEqual(tree, 3, 'かけ')
 
-    def test_000_something(self):
-        """Test something."""
+        tree = self.kongming.cabocha.parse(self.text2)
+        self.assertModifierEqual(tree, 0, '鶏')
+        self.assertModifierEqual(tree, 1, 'もも肉')
+        self.assertModifierEqual(tree, 2, '適当')
+        self.assertModifierEqual(tree, 3, '大きさ')
+        self.assertModifierEqual(tree, 4, '切っ')
+        self.assertModifierEqual(tree, 5, '酒・醤油')
+        self.assertModifierEqual(tree, 6, '漬け')
 
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'kongming.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+    # we don't care about the last chunk because it doesn't modify other chunks
+    def test_get_function(self):
+        tree = self.kongming.cabocha.parse(self.text1)
+        self.assertFunctionEqual(tree, 0, 'が')
+        self.assertFunctionEqual(tree, 1, 'たら')
+        self.assertFunctionEqual(tree, 2, 'を')
+        self.assertFunctionEqual(tree, 3, 'て')
+
+        tree = self.kongming.cabocha.parse(self.text2)
+        self.assertFunctionEqual(tree, 0, 'の')
+        self.assertFunctionEqual(tree, 1, 'を')
+        self.assertFunctionEqual(tree, 2, 'な')
+        self.assertFunctionEqual(tree, 3, 'に')
+        self.assertFunctionEqual(tree, 4, 'て')
+        self.assertFunctionEqual(tree, 5, 'に')
+        self.assertFunctionEqual(tree, 6, 'ておく')
+
+    # we don't care about the first chunk because it isn't modified by other chunks
+    def test_get_head(self):
+        tree = self.kongming.cabocha.parse(self.text1)
+        self.assertHeadEqual(tree, 1, '揚がる')
+        self.assertHeadEqual(tree, 2, '香味ソース')
+        self.assertHeadEqual(tree, 3, 'かける')
+        self.assertHeadEqual(tree, 4, '出来上がる')
+
+        tree = self.kongming.cabocha.parse(self.text2)
+        self.assertHeadEqual(tree, 1, 'もも肉')
+        self.assertHeadEqual(tree, 2, '適当')
+        self.assertHeadEqual(tree, 3, '大きさ')
+        self.assertHeadEqual(tree, 4, '切る')
+        self.assertHeadEqual(tree, 5, '酒・醤油')
+        self.assertHeadEqual(tree, 6, '漬ける')
+        self.assertHeadEqual(tree, 7, '（1時間）')
+
+        tree = self.kongming.cabocha.parse(self.text3)
+        self.assertHeadEqual(tree, 1, '出来上り')
+
+if __name__ == "__main__":
+    unittest.main()
